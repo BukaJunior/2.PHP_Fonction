34bebd8163b74b231052f66f4ddb55d7c5960aca
fabioclock <fabio@oclock.io>
2022-12-19 09:44:48 +0100

    Correction des salles du cinéma

--- php/ensalle.php
+++ php/ensalle.php
@@ -22,6 +22,14 @@ $films = [
     "Yesterday" // 10
 ];
 
+// Tableau contenant la liste des salles
+$rooms = [
+    'Athéna', // => index 0
+    'Dyonisos', // => index 1
+    'Hadès', // => index 2
+    'Zeus' // => index 3
+];
+
 
 // Pour aller chercher une information un tableau, on utilise la notation suivante : $tableau[index]
 // Exemple : echo $films[0] affichera "Avatar 2"
@@ -37,6 +45,21 @@ $films = [
         <li class="film"><?php echo $films[$index] ?></li>
 
     <?php } ?>
+
+</ul>
+
+
+
+<h2 class="page__title">Liste des salles</h2>
+
+<ul>
+    <!-- Syntaxe alternative de for. On remplace les accolades ouvrantes et fermantes par le signe ":" et le mot "endfor". Ça rend l'écriture plus compréhensible quand on mélange PHP et HTML. Fonctionne aussi avec if (endif) ou while (endwhile)  -->
+    <?php for ($indexRoom = 0; $indexRoom <= 3 ; $indexRoom++) : ?> 
+        <!-- Affichage de l'information avec un short echo tag, une version raccourcie du echo. On remplace le mot php et le mot echo par un unique signe = -->
+        <li><?= $rooms[$indexRoom]; ?></li>
+
+    <?php endfor; ?>
+
 
 </ul>
 
