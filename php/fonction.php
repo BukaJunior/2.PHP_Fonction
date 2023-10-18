35c9e244c228a744774c28a0e0e54a44c18856e4
BukaJunior <juniorbuka@gmail.com>
2023-10-18 21:50:24 +0200

    utilisation d'une fonction native PHP COUNT

---
+++ php/fonction.php
@@ -0,0 +1,24 @@
+<?php
+
+$tableau = [
+    'test1',
+    'test2',
+    'test3',
+];
+
+// Fonction native de PHP, permet de compter le nombre d'éléments dans un tableau
+count($tableau); // 3
+
+// On va récuperer cet information dans une variable, pour pouvoir l'utiliser plus tard.
+$resultat = count($tableau);
+
+// On peut ensuite afficher le résultat
+echo $resultat; // 3
+
+
+
+
+
+
+
+?>
